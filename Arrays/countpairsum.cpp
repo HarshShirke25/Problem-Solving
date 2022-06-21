b73156ba61a5e95e2795651f32bb7daf0875738f
HarshShirke25 <shirkeharsh8@gmail.com>
2022-06-21 18:12:36 +0530

    countpairsum

---
+++ Arrays/countpairsum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <unordered_map>
+using namespace std;
+
+int countpairsum(int arr[],int n,int k)
+{
+	unordered_map<int,int> m;
+	int ans = 0;
+	for(int i = 0; i < n; i++)
+	{
+		int b = k - arr[i];
+		if(m[b]){
+			ans+=m[b];
+		}
+		m[arr[i]]++;
+	}
+	return ans;
+}
+
+
+int main()
+{
+	int arr[] = {1, 5, 7, 1};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int k = 6;
+	
+	int cnt = countpairsum(arr,size,k);
+	cout<<"Count is "<<cnt;
+	
+}
